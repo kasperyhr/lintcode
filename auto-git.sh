cf407fbae1881e9e05630a57f135d598e24e7a2c
KasperYang <kasperyhr@gmail.com>
2019-03-05 14:35:50 -0500

    modified auto-git

--- auto-git.sh
+++ auto-git.sh
@@ -1,3 +1,3 @@
-cd /d/study/leetcode
+cd /d/study/lintcode
 git ls-files --others --modified --exclude-standard | while read line ; do IFS='/' read -r -a array <<< "$line"; git add "${array[0]}/"; git commit -m "Problem ${array[0]}"; done
 git push origin master
